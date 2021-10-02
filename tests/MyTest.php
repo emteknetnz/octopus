f04acc0f82d64d7ca75b1de06c7c591035600f63
Steve Boyd <emteknetnz@gmail.com>
2021-10-03 12:27:50 +1300

    .

---
+++ tests/MyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace emteknetnz\Octopus;
+
+use SilverStripe\Dev\SapphireTest;
+
+class MyTest extends SapphireTest
+{
+    public function testItWorks()
+    {
+        assertTrue(true);
+    }
+}
