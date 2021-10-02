eabe3d6877ae355fd7e1496c1ba7dd95835d0889
Steve Boyd <emteknetnz@gmail.com>
2021-10-03 12:33:43 +1300

    .

--- tests/MyTest.php
+++ tests/MyTest.php
@@ -3,11 +3,13 @@
 namespace emteknetnz\Octopus;
 
 use SilverStripe\Dev\SapphireTest;
+use emteknetnz\Octopus\MyDataObject;
 
 class MyTest extends SapphireTest
 {
     public function testItWorks()
     {
-        assertTrue(true);
+        $obj = MyDataObject::create(['Title' => 'MyTitle'])->write();
+        assertSame('MyTitle', MyDataObject::get()->first()->Title);
     }
 }
