f06a5d37d5121cf71e287edc2e42f793f6cb3d35
Steve Boyd <emteknetnz@gmail.com>
2021-10-04 11:27:29 +1300

    .

--- .github/things/apache2.php
+++ .github/things/apache2.php
@@ -4,3 +4,11 @@ $s = file_get_contents('.github/things/000-default.conf');
 #$s = file_get_contents('/etc/apache2/sites-enabled/000-default.conf');
 $s = str_replace('$pwd', $pwd, $s);
 file_put_contents('000-default.conf', $s);
+
+$s = file_get_contents('/etc/apache2/envvars');
+$s .= "\n";
+$s .= "export APACHE_RUN_USER=runner";
+$s .= "\n";
+$s .= "export APACHE_RUN_GROUP=docker";
+$s .= "\n";
+file_put_contents('envvars', $s);
