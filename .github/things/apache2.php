a2a0c6699dcfd6285fdba183fa9b53aafb5d6745
Steve Boyd <emteknetnz@gmail.com>
2021-10-03 17:31:05 +1300

    .

--- .github/things/apache2.php
+++ .github/things/apache2.php
@@ -1,5 +1,6 @@
 <?php
 $pwd = shell_exec('pwd');
-$a = file_get_contents('.github/things/docker_apache_default');
-$a = str_replace('/var/www', $pwd, $a);
-file_put_contents('/etc/apache2/sites-enabled/000-default.conf', $a);
+$fn = '/etc/apache2/sites-enabled/000-default.conf';
+$s = file_get_contents($fn);
+$s = str_replace('/var/www/html', $pwd, $s);
+file_put_contents($fn, $s);
