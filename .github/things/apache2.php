d6bfba0009e2d1ff6a6831e7a35a9c9c2403f88e
Steve Boyd <emteknetnz@gmail.com>
2021-10-03 17:35:44 +1300

    .

--- .github/things/apache2.php
+++ .github/things/apache2.php
@@ -1,6 +1,5 @@
 <?php
 $pwd = shell_exec('pwd');
-$fn = '/etc/apache2/sites-enabled/000-default.conf';
-$s = file_get_contents($fn);
+$s = file_get_contents('/etc/apache2/sites-enabled/000-default.conf');
 $s = str_replace('/var/www/html', $pwd, $s);
-file_put_contents($fn, $s);
+file_put_contents('000-default.conf', $s);
