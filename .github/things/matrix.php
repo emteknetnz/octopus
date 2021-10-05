92922d40d5cdfdf29d7ebcddc53a3188e3c3b07b
Steve Boyd <emteknetnz@gmail.com>
2021-10-06 00:21:22 +1300

    .

--- .github/things/matrix.php
+++ .github/things/matrix.php
@@ -1,39 +1,23 @@
 <?php
 # Reads the yml matrix for ci.yml and inputs.txt and creates a new json matrix
 $yml = file_get_contents('ci.yml');
-// fix really weird bug in yaml_parse where 'on' key is turned into '1'
-$yml = str_replace("\non:", "\nonx:", $yml);
 $y = yaml_parse($yml);
-$ci_matrix = $y['jobs']['metadata']['strategy']['matrix']['include'];
-$ci_inputs = $y['onx']['workflow_call']['inputs'];
-$module_inputs = [];
-foreach (explode("\n", file_get_contents('module_inputs.txt')) as $line) {
-    $a = preg_split('#=#', $line);
-    if (isset($a[1])) {
-        $module_inputs[$a[0]] = $a[1];
-    }    
-}
-
-$tests = [];
-foreach ($ci_inputs as $input => $arr) {
-    $include = $arr['default'];
-    if (isset($module_inputs[$input])) {
-        $include = $module_inputs[$input];
-    }
-    if ($include) {
-        $test = str_replace('run_', '', $input); // e.g. run_phplint => phplint
-        $tests[$test] = true; 
-    }
+$matrix = $y['jobs']['metadata']['strategy']['matrix']['include'];
+$includes = [];
+foreach (explode("\n", file_get_contents('inputs.txt')) as $line) {
+    if (empty($line)) continue;
+    list($input, $do_include) = preg_split('#=#', $line);
+    if (!$do_include) continue;
+    $test = str_replace('run_', '', $input); // e.g. run_phplint => phplint
+    $includes[$test] = true; 
 }
 
 $out = ['jobs'=>['metadata'=>['strategy'=>['matrix'=>['include']]]]];
 
-foreach ($ci_matrix as $arr) {
+foreach ($matrix as $arr) {
     foreach (array_keys($arr) as $test) {
-        if ($test == 'php') {
-            continue;
-        }
-        if (isset($tests[$test])) {
+        if ($test == 'php') continue;
+        if (isset($includes[$test]) && $includes[$test]) {
             $out['jobs']['metadata']['strategy']['matrix']['include'][] = $arr;
         }
     }
