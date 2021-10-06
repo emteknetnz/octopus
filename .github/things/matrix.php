6effb469d49de67e13bc002408685dd7fb8ced15
Steve Boyd <emteknetnz@gmail.com>
2021-10-06 16:53:12 +1300

    .

--- .github/things/matrix.php
+++ .github/things/matrix.php
@@ -1,26 +1,43 @@
 <?php
-# Reads the yml matrix for ci.yml and inputs.txt and creates a new json matrix
-$yml = file_get_contents('ci.yml');
-$y = yaml_parse($yml);
-$matrix = $y['jobs']['metadata']['strategy']['matrix'];
-$includes = [];
-foreach (explode("\n", file_get_contents('inputs.txt')) as $line) {
-    if (empty($line)) continue;
-    list($input, $do_include) = preg_split('#=#', $line);
-    $do_include = $do_include == 'true';
-    $test = str_replace('run_', '', $input); // e.g. run_phplint => phplint
-    $includes[$test] = $do_include; 
-}
 
-$new_matrix = ['include' => []];
-foreach ($matrix['include'] as $arr) {
-    foreach (array_keys($arr) as $test) {
-        if ($test == 'php' || !isset($includes[$test]) || !$includes[$test]) continue;
-        $new_matrix['include'][] = $arr;
+// Reads inputs.yml and creates a new json matrix
+
+$inputs = yaml_parse(file_get_contents('inputs.yml'));
+$runTests = [];
+$defaultJobs = [];
+$extraJobs = [];
+foreach ($inputs as $input => $value) {
+    if (preg_match('#^run_#', $input)) {
+        if ($value === 'true') {
+            $value = true;
+        }
+        if ($value === 'false') {
+            $value = false;
+        }
+        // e.g. run_phplinting => phplinting
+        $test = str_replace('run_', '', $input);
+        $runTests[$test] = $value;
+    } else if ($input === 'default_jobs') {
+        $defaultJobs = $value;
+    } else if ($input === 'extra_jobs') {
+        $extraJobs = $value;
     }
 }
 
-$json = json_encode($new_matrix);
+$matrix = ['include' => []];
+foreach ($defaultJobs as $arr) {
+    foreach (array_keys($arr) as $test) {
+        if ($test === 'php' || !isset($runTests[$test]) || !$runTests[$test]) {
+            continue;
+        }
+        $matrix['include'][] = $arr;
+    }
+}
+foreach ($extraJobs as $arr) {
+    $matrix['include'][] = $arr;
+}
+
+$json = json_encode($matrix);
 $json = preg_replace("#\n +#", "\n", $json);
 $json = str_replace("\n", '', $json);
 echo trim($json);
